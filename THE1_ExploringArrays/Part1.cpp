ec50b97694d33b0168c1fe08e685786e28d5a15c
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2026-01-29 03:47:30 +0800

    Create Part1.cpp

---
+++ THE1_ExploringArrays/Part1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+int main() {
+    
+    int arr[10] = {7, 9, 20, 5, 2, 8, 19, 6, 3, 1};
+    int sum = 0;
+    int largest, secondLargest, smallest, secondSmallest;
+    
+    std::cout << "Array: ";
+    for(int i = 0; i < 10; i++) {
+        std::cout << arr[i] << " ";
+        sum += arr[i];
+    }
+    
+    std::cout << "\nSum: "<< sum;
+    std::cout << "\nAverage: " << sum / 10;
+    
+    if(arr[0] < arr[1]) {
+        largest = arr[1];
+        secondLargest = arr[0];
+    }
+    
+    else{
+        largest = arr[0];
+        secondLargest = arr[1];
+    }
+    
+    for(int i = 2; i < 10; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+      }
+    }
+    
+    std::cout << "\nSecond Largest: " << secondLargest;
+    
+    std::cout << "\nReversed Array: ";
+    for(int i = 9; i >= 0; i--){
+        std::cout << arr[i] << " ";
+    }
+    
+    
+    
+
+    return -1;
+}
