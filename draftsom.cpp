e830967fd53ceef79aee1265610183a7a58c16c1
ktrzl1ane <kocteza@gbox.adnu.edu.ph>
2026-03-03 22:29:59 +0800

    Update draftsom.cpp

--- draftsom.cpp
+++ draftsom.cpp
@@ -132,7 +132,7 @@ void userMenu(vector<string>& menuItems,
                         cout << "Enter payment amount: ";
                         cin >> payment;
                         if(payment < total)
-                            cout << "Insufficient. Please enter at least " << total;
+                            cout << "Insufficient. Please enter at least " << total << ".\n";
                     } while(payment < total);
                 
                     double change = payment - total;
@@ -157,7 +157,73 @@ void userMenu(vector<string>& menuItems,
             else {
                 receiptItems.clear();
                 receiptPrices.clear();
+                receiptQuantities.clear();
                 cout << "Receipt cleared successfully.\n";
+            }
+        }
+        
+        
+        // Task 4:
+        else if(choice == 5) {
+            int index;
+            
+            if(receiptItems.empty()) {
+                cout << "Receipt is empty.\n";
+            }
+            
+            else {
+                cout << "=== CURRENT ITEMS IN RECEIPT ===" << endl;
+                for(int i = 0; i < receiptItems.size(); i++) {
+                    cout << i << ". " << receiptItems[i] << " x" << receiptQuantities[i] <<" - Php" << receiptPrices[i] << endl;
+                }
+                cout << "================================" << endl;
+            
+                cout << "Enter receipt index to remove: ";
+                cin >> index;
+            
+                if(index >= 0 && index < receiptItems.size()) {
+                    
+                    int quantity = receiptQuantities[index];
+                    int decision;
+                    
+                    if(quantity > 1) {
+                        cout << "Press: 1 - Reduce quantity by 1.\n";
+                        cout << "       0 - Remove item entirely.\n";
+                        cout << "Enter choice: ";
+                        cin >> decision;
+                        
+                        if (decision == 1) {
+                            receiptQuantities[index]--;
+                            cout << "Successfully reduced quantity by 1.\n";
+                        }
+                        
+                        else if(decision == 0) {
+                            receiptItems.erase(receiptItems.begin() + index);
+                            receiptPrices.erase(receiptPrices.begin() + index);
+                            receiptQuantities.erase(receiptQuantities.begin() + index);
+                            cout << "Item removed successfully.\n";
+                        }
+                        
+                        else {
+                            cout << "Invalid input.\n";
+                        }
+                    }
+                    
+                    else if(quantity == 1) {
+                        receiptItems.erase(receiptItems.begin() + index);
+                        receiptPrices.erase(receiptPrices.begin() + index);
+                        receiptQuantities.erase(receiptQuantities.begin() + index);
+                        cout << "Item removed successfully.\n";
+                    }
+                    
+                    else {
+                        cout << "Invalid input.\n";
+                    }
+                }
+            
+                else {
+                    cout << "Invalid input.\n";
+                }
             }
         }
 
@@ -177,7 +243,8 @@ void adminMenu(vector<string>& menuItems,
 		cout << "\n1. View Menu\n";
 		cout << "2. Add New Menu Item\n";
 		cout << "3. Change Admin Passcode\n";
-		cout << "4. Exit\n";
+		cout << "4. Edit Existing Menu Item\n";
+		cout << "5. Exit\n";
 		cout << "Enter choice: ";
 		cin >> choice;
 		
@@ -187,7 +254,7 @@ void adminMenu(vector<string>& menuItems,
 		    displayMenu(menuItems, menuPrices);
 		}
 
-		if(choice == 2) {
+		else if(choice == 2) {
             string newItem; double newPrice;
             cin.ignore();
             cout << "Enter item name: ";
@@ -216,10 +283,69 @@ void adminMenu(vector<string>& menuItems,
 		    else {
 		        cout << "Incorrect current passcode.\n";
 		    }
-		   
 		}
 		
-	} while(choice != 4);
+		//Task 5:
+		else if(choice == 4) {
+		    
+            displayMenu(menuItems, menuPrices);
+		    
+            int index;
+            cout << "\nEnter what item index to edit: ";
+            cin >> index;
+            
+            if(index >= 0 && index < menuItems.size()) {
+                
+                int decision;
+                cout << "Edit: 1 - Name\n";
+                cout << "      2 - Price\n";
+                cout << "      3 - Both\n";
+                cout << "Enter choice: ";
+                cin >> decision;
+                
+                if(decision == 1) {
+                    string newName;
+                    cin.ignore();
+                    cout << "Enter New Name: ";
+                    getline(cin, newName);
+                    menuItems[index] = newName;
+                    cout << "Successfully edited item name.\n";
+                }
+                
+                else if(decision == 2) {
+                    double newPrice;
+                    cout << "Enter New Price: ";
+                    cin >> newPrice;
+                    menuPrices[index] = newPrice;
+                    cout << "Successfully edited item price.\n";
+                }
+                
+                else if(decision == 3) {
+                    string newName;
+                    cin.ignore();
+                    cout << "Enter New Name: ";
+                    getline(cin, newName);
+                    menuItems[index] = newName;
+                    
+                    double newPrice;
+                    cout << "Enter New Price: ";
+                    cin >> newPrice;
+                    menuPrices[index] = newPrice;
+                    
+                    cout << "Successfully edited item name and price.\n";
+                }
+                
+                else {
+                    cout << "Invalid input.\n";
+                }
+            }
+            
+            else {
+                cout << "Invalid input.\n";
+            }
+		}
+		
+	} while(choice != 5);
 }
 
 int main() {
